fd52191e5271d4ed6ee7878af563e674ce2d4bb7
Stefan K <120093408+stefan0307@users.noreply.github.com>
2023-02-28 16:05:20 +0100

    Add files via upload

---
+++ Homework1_C#/Homework1_C#/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Homework1_C_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Without command for new line
+            Console.WriteLine("    *");
+            Console.WriteLine("   ***");
+            Console.WriteLine("  *****");
+            Console.WriteLine(" *******");
+            Console.WriteLine("*********");
+            
+            //With command for new line (\r\n is for Windows, \r is for Mac and \n is for Unix)
+            Console.WriteLine("    *\r\n   ***\r\n  *****\r\n *******\r\n*********");
+        }
+    }
+}
