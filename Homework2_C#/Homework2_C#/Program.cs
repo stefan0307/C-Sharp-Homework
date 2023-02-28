47c393b2ba079f29c85dd18584d9d2996b4a29ac
Stefan K <120093408+stefan0307@users.noreply.github.com>
2023-02-28 16:06:19 +0100

    Add files via upload

---
+++ Homework2_C#/Homework2_C#/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Homework2_C_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Without \r\n
+            Console.WriteLine("    *   ");
+            Console.WriteLine("    *   ");
+            Console.WriteLine("  * * * ");
+            Console.WriteLine("*   *   *");
+            Console.WriteLine("    *   ");
+            Console.WriteLine("    *   ");
+            Console.WriteLine("  *   * ");
+            Console.WriteLine(" *     * ");
+
+            //With \r\n
+            Console.WriteLine("    *   \r\n    *   \r\n  * * * \r\n*   *   *\r\n    *   \r\n    *   \r\n  *   * \r\n *     * ");
+        }
+    }
+}
