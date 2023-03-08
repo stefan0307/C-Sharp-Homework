06f463ec4faa210905e5d91b75db91465761c308
Stefan K <120093408+stefan0307@users.noreply.github.com>
2023-03-08 13:39:00 +0100

    Add files via upload

---
+++ Homework Class 3/Homework Class 3/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Homework_Class_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] SumOfEven = new int[6];
+            for (int i = 0; i<SumOfEven.Length; i++)
+            {
+                Console.WriteLine("Enter integer number " + (i + 1));
+                bool test = int.TryParse(Console.ReadLine(), out SumOfEven[i]);
+                while (!test) { 
+                    if (!test)
+                    {
+                    Console.WriteLine("Invalid input!");
+                    Console.WriteLine("Enter integer number " + (i + 1));
+                    test = int.TryParse(Console.ReadLine(), out SumOfEven[i]);
+                    }
+                }
+            }
+
+            int result = 0;
+            for (int i = 0;  i < SumOfEven.Length; i++)
+            {
+                if (SumOfEven[i] % 2 == 0)
+                {
+                    result += SumOfEven[i];
+                }
+            }
+            Console.WriteLine("The result is: " + result);
+        }
+    }
+}
---
+++ Homework Class 3/StudentGroup/Program.cs
@@ -0,0 +1,61 @@
+﻿namespace StudentGroup
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Press 1 to see students in G1. Press 2 to see students in G2:");
+
+            string[] studentsG1 = new string[] {"Stefan", "Filip", "Boshko", "Angela", "Bojana"};
+            string[] studentsG2 = new string[] { "Ivana", "Gorazd", "Ljupco", "Matea", "Mihaela" };
+
+            bool userInput = int.TryParse(Console.ReadLine(), out int classNumber);
+            if (!userInput || classNumber < 1 || classNumber > 2)
+            {
+                Console.WriteLine("Invalid Input!");
+
+            }else
+            {
+                if (classNumber == 1)
+                {
+                    Console.WriteLine("The Students in G1 are:");
+                    for (int i = 0; i < studentsG1.Length; i++)
+                    {
+                        Console.WriteLine(studentsG1[i]);
+                    }
+                }
+                else if (classNumber == 2)
+                {
+                    Console.WriteLine("The Students in G1 are:");
+                    for (int i = 0; i < studentsG2.Length; i++)
+                    {
+                        Console.WriteLine(studentsG2[i]);
+                    }
+                }
+            }
+            
+
+            //string userInput = Console.ReadLine();
+            //switch (userInput)
+            //{
+            //    case "1":
+            //        Console.WriteLine("The Students in G1 are:");
+            //        for (int i = 0; i < studentsG1.Length; i++)
+            //        {
+            //            Console.WriteLine(studentsG1[i]);
+            //        }
+            //        break;
+            //    case "2":
+            //        Console.WriteLine("The Students in G1 are:");
+            //        for (int i = 0; i < studentsG2.Length; i++)
+            //        {
+            //            Console.WriteLine(studentsG2[i]);
+            //        }
+            //        break;
+            //    default: 
+            //        Console.WriteLine("Invalid Input!");
+            //        break;
+            //}
+        }
+    }
+}
